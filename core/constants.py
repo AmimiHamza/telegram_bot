a9222333e8abb57de22ba1fafe7ec97ec610dd07
Earthyy <elardi2001@gmail.com>
2024-02-14 16:49:12 +0000

    add constants file

---
+++ core/constants.py
@@ -0,0 +1,23 @@
+GREETING=['hello','hi','hey','slm','salam']
+BOT_USERNAME='@the_dse_bot'
+commands=['get','show','add','delete','update']
+
+HELP_COMMAND_SUMMARY= """
+🤖 Bot commands:
+
+/start - bot introduction
+/help - bot commands summary
+/get - get study material documents by course name, type, year and part. for example, if you are looking for RG course study materials, you can use the following command: /get rg cours
+
+For any questions or suggestions, please contact the bot admin.
+"""
+
+GET_COMMAND_SUMMARY = """
+GET COMMAND SUMMARY
+"""
+
+
+BOT_INTRODUCTION_MESSAGE = """
+BOT INTRODUCTION MESSAGE
+"""
+
